b94ad3affcd6c3f34f822e41629610a8e04d7c8b
svyatsharik <sharov.so@phystech.edu>
2024-05-17 20:17:00 +0300

    Добавил фиксацию меню при скролле

--- js/script.js
+++ js/script.js
@@ -42,3 +42,19 @@ function popap() {
 
 popap();
 countdown();
+
+const menu = document.querySelector('.menu');
+const header = document.querySelector('.header');
+let k = 0;
+window.addEventListener('scroll', function() {
+    console.log(window);
+    if (menu.getBoundingClientRect().y <= 0 && k === 0) {
+        menu.style.position = 'fixed';
+        menu.style.top = 0;
+        menu.style.left = 0;
+        k = 1;
+    } else if (header.getBoundingClientRect().y + header.getBoundingClientRect().height >= 0 && k === 1) {
+        menu.style.position = 'static';
+        k = 0;
+    }
+  });
